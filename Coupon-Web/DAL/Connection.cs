c8c8a9e287279dff1cd5cc06465432c551a50f06
Tomer Ben Tolila <tomerbt3@gmail.com>
2015-04-16 12:39:58 +0300

    commit

---
+++ Coupon-Web/DAL/Connection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace Coupon_Web.DAL
+{
+    public class Connection
+    {
+
+
+        private const string URL = @"Data Source=(LocalDB)\v11.0;
+                            AttachDbFilename=C:\Users\asus\Documents\coupon-web\Coupon-Web\App_Data\LocalDB.mdf;
+                            Integrated Security=True";
+
+        private SqlConnection _conn;
+        public Connection()
+        {
+            _conn = new System.Data.SqlClient.SqlConnection();
+            _conn.ConnectionString = URL;
+        }
+
+        public SqlConnection getSqlCon()
+        {
+            return _conn;
+        }
+        public void open()
+        {
+            _conn.Open();
+        }
+        public void close()
+        {
+            _conn.Close();
+        }
+    }
+}
---
+++ Coupon-Web/test.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using Coupon_Web.DAL;
+
+namespace Coupon_Web.PL
+{
+    public partial class test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Connection conn = new Connection();
+            
+            SqlCommand cmdSql = new SqlCommand("insert into [users] values (@UserName,@Name,@Password)", conn.getSqlCon());
+           
+            conn.open();
+
+            cmdSql.Parameters.AddWithValue("@UserName", "shimi2");
+            cmdSql.Parameters.AddWithValue("@Name", "shimisv");
+            cmdSql.Parameters.AddWithValue("@Password", "1234");
+
+            cmdSql.ExecuteNonQuery();
+            Label1.Text = "added";
+            conn.close();
+        }
+    }
+}
