57b2a71d1dfdda973c2102b599d56ef71a6b11e7
Tomer Ben Tolila <tomerbt3@gmail.com>
2015-04-19 19:30:58 +0300

    update url for localdb

--- Coupon-Web/DAL/Connection.cs
+++ Coupon-Web/DAL/Connection.cs
@@ -9,7 +9,7 @@ namespace Coupon_Web.DAL
     public class Connection
     {
         private const string URL = @"Data Source=(LocalDB)\v11.0;
-                            AttachDbFilename=C:\Users\shimi\Documents\coupon-web\Coupon-Web\App_Data\LocalDB.mdf;
+                            AttachDbFilename=|DataDirectory|\LocalDB.mdf;
                             Integrated Security=True";
 
         private SqlConnection _conn;
